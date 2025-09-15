486bfd2e2607c02cbe1c93c1a3a7ad1dbe63cd71
BenjaminC188 <benjaminc188@nycstudents.net>
2025-09-15 06:26:31 -0700

    s

---
+++ test copy.py
@@ -0,0 +1,51 @@
+import turtle
+from turtle import *
+
+t = Turtle()
+
+for i in range(4):
+    t.forward(100)
+    t.left(90)
+
+def triangle(x):
+    for i in range(3):
+        t.forward(x)
+        t.left(120)
+
+
+def square(x):
+    for i in range(4):
+        t.forward(x)
+        t.left(90)
+
+#for i in range(60):
+    square(100)
+    t.right(5)
+
+sidelength = 100
+rotate = 90
+
+sidelength = 100
+rotate = 90
+def square(x,y):
+    for i in range(4):
+        t.forward(x)
+        t.left(y)
+#triangle(100,90)
+
+def doubleSquares(iRange):
+    length = 25
+    for i in range(iRange):
+        square(length, 90)
+        length = length * 2
+#doubleSquares(5)
+
+def addSquares(iRange):
+    length = 25
+    for i in range(iRange):
+        square(length, 90)
+        length += 25
+addSquares(5)
+
+
+
--- test.py
+++ test.py
@@ -1,1 +1,55 @@
-print("hello")
+import turtle
+from turtle import *
+t = Turtle()
+
+t.shape('turtle')
+
+#t.forward(200)
+
+def message(input):
+    print(input)
+#message("Hello Class")
+
+def square(x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+#square(200)
+
+
+def equal(x):
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+#equal(200) 
+
+def right():
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(135)
+    t.forward(142)
+#right()
+
+def rect():
+    t.forward(100)
+    t.left(90)
+    t.forward(125)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(125)
+    t.left(90)
+#rect()
+
+#equal(90)
+
+turtle.done()
+
